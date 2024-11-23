d247cc74cc9f494d0cf71537c441ee97eb153905
TVNSource <vanthinh@gmail.com>
2024-11-23 16:16:49 -0600

    Update homework4.js

--- homework4.js
+++ homework4.js
@@ -596,7 +596,7 @@ function createDynamicCheckbox(firstName) {
     checkbox.addEventListener("change", () => {
         if (checkbox.checked) {	
 			myTest();		
-            deleteCookie("firstName");			
+            deleteCookie("fName");			
 			document.getElementById("registrationForm").reset();    
 			console.log("cookied delete & form reset");
             displayNameOnHeader();
